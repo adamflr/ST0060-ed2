b9b7c7bc270925df0eb3b6de356b0c108cb66bc4
adamflr <flr.adam@gmail.com>
2025-10-06 10:51:33 +0200

    Minor changes ch 2

--- Skriptversioner/extract-script.R
+++ Skriptversioner/extract-script.R
@@ -6,7 +6,7 @@ for(i in 1:4){
   
   txt <- readLines(paste0("Skriptversioner/kap-", i, ".R"))
   txt <- txt[substr(txt, 1, 5) != "## --"]
-  txt[-intersect(which(txt[-1] == txt[-length(txt)]) + 1, which(txt == ""))]
+  txt <- txt[-intersect(which(txt[-1] == txt[-length(txt)]) + 1, which(txt == ""))]
   
   writeLines(txt, paste0("Skriptversioner/kap-", i, ".R"))
 }
